0aa654dd9d83553611df5461f0e470b541d6b417
joao <joaov.dias08@gmail.com>
2025-05-09 21:56:32 -0300

    starting rr class

--- src/main/java/com/os/RoundRobin.java
+++ src/main/java/com/os/RoundRobin.java
@@ -6,5 +6,10 @@ public class RoundRobin {
     public static void main(List<Processo> processoList){
         // Setar Quantum
         int quantum = 2;
+        int timeline = 0;
+
+        while(processoList != null){
+            
+        }
     }
 }
